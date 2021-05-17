6a328b6426d6e449982ff9967229aa48a7ecd6b8
117649 <lhw.1012@gmail.com>
2021-05-18 02:51:27 +0800

    manual pref optional warring hide

--- resource/defaults.js
+++ resource/defaults.js
@@ -74,12 +74,12 @@ prefList = {
 };
 
 // If we're initializing in a content process, we don't care about the rest
-if(isContent) { throw 'isContent'; }
+if (isContent) { throw 'isContent'; }
 
 paneList = [
-	[ "paneTabGroups", true ],
-	[ "paneHowTo", true ],
-	[ "paneSession", true ]
+	["paneTabGroups", true],
+	["paneHowTo", true],
+	["paneSession", true]
 ];
 
 function startAddon(window) {
@@ -99,12 +99,12 @@ function backupCurrentSession() {
 
 	Cu.importGlobalProperties(['TextEncoder'])
 
-	let prefix = objName+'-update.js-';
+	let prefix = objName + '-update.js-';
 
 	// We can use the initTime as a seed/identifier to make sure every file has a unique name.
 	// This is the same suffix syntax as the automated backups created by Firefox upgrades, except it uses a buildID instead
 	// (which we don't have for the add-on, hence initTime instead).
-	let filename = prefix+AddonData.initTime;
+	let filename = prefix + AddonData.initTime;
 
 	// This is the folder where the automated backups created by Firefox upgrades are saved.
 	let profileDir = osfile.OS.Constants.Path.profileDir;
@@ -123,42 +123,41 @@ function backupCurrentSession() {
 			let iterator = new osfile.OS.File.DirectoryIterator(backupsDir);
 			iterating = iterator.forEach((file) => {
 				// a copy of the current session, for crash-protection
-				if(file.name.startsWith(prefix)) {
+				if (file.name.startsWith(prefix)) {
 					let val = parseInt(file.name.substr(prefix.length));
 					existingBackups.push(val);
 				}
 			});
 			iterating.then(
-				function() {
+				function () {
 					iterator.close();
 					let max = Services.prefs.getIntPref('browser.sessionstore.upgradeBackup.maxUpgradeBackups');
-					if(existingBackups.length > max) {
+					if (existingBackups.length > max) {
 						// keep the most recently created files
-						existingBackups.sort(function(a,b) { return b-a; });
+						existingBackups.sort(function (a, b) { return b - a; });
 						let toRemove = existingBackups.splice(3);
-						for(let seed of toRemove) {
-							let name = prefix+seed;
+						for (let seed of toRemove) {
+							let name = prefix + seed;
 							let path = osfile.OS.Path.join(backupsDir, name);
 							osfile.OS.File.remove(path);
 						}
 					}
 				},
-				function(reason) { iterator.close(); throw reason; }
+				function (reason) { iterator.close(); throw reason; }
 			);
 		});
 	});
 }
 
-async function onInstall(aData) {
-	(await AddonManager.getAddonByID(`${aData.id}`)).__AddonInternal__.signedState = AddonManager.SIGNEDSTATE_NOT_REQUIRED;
+function onInstall(aData) {
 }
 
-function onStartup() {
+async function onStartup(aData) {
 	// If this is the first startup after installing or updating the add-on, make a backup of the session, just in case.
-	if(STARTED == ADDON_INSTALL || STARTED == ADDON_UPGRADE || STARTED == ADDON_DOWNGRADE) {
+	if (STARTED == ADDON_INSTALL || STARTED == ADDON_UPGRADE || STARTED == ADDON_DOWNGRADE) {
 		// Don't block add-on startup if it fails to create the backup for some reason.
 		try { backupCurrentSession(); }
-		catch(ex) { Cu.reportError(ex); }
+		catch (ex) { Cu.reportError(ex); }
 	}
 
 	Modules.load('Utils');
@@ -174,6 +173,12 @@ function onStartup() {
 	// Apply the add-on to every window opened and to be opened
 	Windows.callOnAll(startAddon, 'navigator:browser');
 	Windows.register(startAddon, 'domwindowopened', 'navigator:browser');
+
+	try {
+		Services.prefs.getBoolPref("extensions." + objPathString + ".hide_warring") ?
+			(await AddonManager.getAddonByID(`${aData.id}`)).__AddonInternal__.signedState = AddonManager.SIGNEDSTATE_NOT_REQUIRED
+			: (await AddonManager.getAddonByID(`${aData.id}`)).__AddonInternal__.signedState === AddonManager.SIGNEDSTATE_NOT_REQUIRED ? (await AddonManager.getAddonByID(`${aData.id}`)).__AddonInternal__.signedState = AddonManager.SIGNEDSTATE_MISSING : '';
+	} catch (error) { }
 }
 
 function onShutdown() {
